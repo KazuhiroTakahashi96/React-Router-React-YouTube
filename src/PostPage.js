84182fd59be08b3e5685a0b2c5d287a2aacbf593
Kazu Takahashi <num96kazu19@icloud.com>
2022-02-06 20:56:07 +0900

    完成形　大変だわw

--- src/PostPage.js
+++ src/PostPage.js
@@ -5,7 +5,25 @@ const PostPage = ({ posts, handleDelete }) => {
   const post = posts.find((post) => post.id.toString() === id);
   return (
     <main className="PostPage">
-      <article className="post"></article>
+      <article className="post">
+        {post && (
+          <>
+            <h2>{post.title}</h2>
+            <p className="postDate">{post.datetime}</p>
+            <p className="postBody">{post.body}</p>
+            <button onClick={() => handleDelete(post.id)}>Delete Post</button>
+          </>
+        )}
+        {!post && (
+          <>
+            <h2>{post.title}Post Not Found</h2>
+            <p>Well, that's disappointng</p>
+            <p>
+              <Link to="/">Visit Our Homepage</Link>
+            </p>
+          </>
+        )}
+      </article>
     </main>
   );
 };
