fdbb6664868514dd4b783913bd127a3f9075ae5a
Kazu Takahashi <num96kazu19@icloud.com>
2022-02-07 14:56:53 +0900

    useContextでよりスマートに

--- src/App.js
+++ src/App.js
@@ -7,142 +7,31 @@ import PostPage from "./PostPage";
 import EditPost from "./EditPost";
 import About from "./About";
 import Missing from "./Missing";
-import { Route, Routes, useNavigate } from "react-router-dom";
-import { useState, useEffect } from "react";
-import { format } from "date-fns";
-import api from "./api/posts";
-import useWindowSize from "./hooks/useWindowSize";
-import useAxiosFetch from "./hooks/useAxiosFetch";
+import { Route, Routes } from "react-router-dom";
+import { DataProvider } from "./context/DataContext";
 
 function App() {
-  const [posts, setPosts] = useState([]);
-  const [search, setSearch] = useState("");
-  const [searchResults, setSearchResults] = useState([]);
-  const [postTitle, setPostTitle] = useState("");
-  const [postBody, setPostBody] = useState("");
-  const [editTitle, setEditTitle] = useState("");
-  const [editBody, setEditBody] = useState("");
-  const navigate = useNavigate();
-  const { width } = useWindowSize();
-  const { data, fetchError, isLoading } = useAxiosFetch(
-    "http://localhost:3500/posts"
-  );
-
-  useEffect(() => {
-    setPosts(data);
-  }, [data]);
-
-  useEffect(() => {
-    const filteredResults = posts.filter(
-      (post) =>
-        post.body.toLowerCase().includes(search.toLowerCase()) ||
-        post.title.toLowerCase().includes(search.toLowerCase())
-    );
-
-    setSearchResults(filteredResults.reverse());
-  }, [posts, search]);
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
-    const datetime = format(new Date(), "MMMM dd, yyyy pp");
-    const newPost = { id, title: postTitle, datetime, body: postBody };
-    try {
-      const response = await api.post("/posts", newPost);
-      const allPosts = [...posts, response.data];
-      setPosts(allPosts);
-      setPostTitle("");
-      setPostBody("");
-      navigate("/");
-    } catch (err) {
-      console.log(`Error: ${err.message}`);
-    }
-  };
-
-  const handleEdit = async (id) => {
-    const datetime = format(new Date(), "MMMM dd, yyyy pp");
-    const updatedPost = { id, title: editTitle, datetime, body: editBody };
-    try {
-      const response = await api.put(`/posts/${id}`, updatedPost);
-      setPosts(
-        posts.map((post) => (post.id === id ? { ...response.data } : post))
-      );
-      setEditTitle("");
-      setEditBody("");
-      navigate("/");
-    } catch (err) {
-      console.log(`Error: ${err.message}`);
-    }
-  };
-
-  const handleDelete = async (id) => {
-    try {
-      await api.delete(`/posts/${id}`);
-      const postsList = posts.filter((post) => post.id !== id);
-      setPosts(postsList);
-      navigate("/");
-    } catch (err) {
-      console.log(`Error: ${err.message}`);
-    }
-  };
-
   return (
     // https://qiita.com/junjis0203/items/0096963aefb70f466c27
     // https://reffect.co.jp/react/react-router-6
     // https://www.youtube.com/watch?v=XBRLVRjZ3CQ
 
-    <Routes>
-      <Route
-        path="/"
-        element={<Layout search={search} setSearch={setSearch} width={width} />}
-      >
-        <Route
-          index
-          element={
-            <Home
-              posts={searchResults}
-              fetchError={fetchError}
-              isLoading={isLoading}
-            />
-          }
-        />
-        <Route path="post">
-          <Route
-            index
-            element={
-              <NewPost
-                handleSubmit={handleSubmit}
-                postTitle={postTitle}
-                setPostTitle={setPostTitle}
-                postBody={postBody}
-                setPostBody={setPostBody}
-              />
-            }
-          />
+    <DataProvider>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="post">
+            <Route index element={<NewPost />} />
 
-          <Route
-            path="edit/:id"
-            element={
-              <EditPost
-                posts={posts}
-                handleEdit={handleEdit}
-                editTitle={editTitle}
-                setEditTitle={setEditTitle}
-                editBody={editBody}
-                setEditBody={setEditBody}
-              />
-            }
-          />
+            <Route path="edit/:id" element={<EditPost />} />
 
-          <Route
-            path=":id"
-            element={<PostPage posts={posts} handleDelete={handleDelete} />}
-          />
+            <Route path=":id" element={<PostPage />} />
+          </Route>
+          <Route path="about" element={<About />} />
+          <Route path="*" element={<Missing />} />
         </Route>
-        <Route path="about" element={<About />} />
-        <Route path="*" element={<Missing />} />
-      </Route>
-    </Routes>
+      </Routes>
+    </DataProvider>
   );
 }
 
