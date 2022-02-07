163c6d6808c53e958aeac96f8211e54361ee7ab5
Kazu Takahashi <num96kazu19@icloud.com>
2022-02-07 11:09:00 +0900

    /が抜けてた これで上手くいった

--- src/PostPage.js
+++ src/PostPage.js
@@ -11,7 +11,7 @@ const PostPage = ({ posts, handleDelete }) => {
             <h2>{post.title}</h2>
             <p className="postDate">{post.datetime}</p>
             <p className="postBody">{post.body}</p>
-            <Link to={`post/edit/${post.id}`}>
+            <Link to={`/post/edit/${post.id}`}>
               <button className="editButton">Edit Post</button>
             </Link>
             <button
